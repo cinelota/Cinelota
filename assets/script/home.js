85085630f2006b25ede85107329c4eec56e9bdaf
Cinelota <nicolasinhuusilvaa@gmail.com>
2025-01-19 18:48:12 -0300

    Add files via upload

--- assets/script/home.js
+++ assets/script/home.js
@@ -1,28 +1,31 @@
-// Seleciona os botões
 const explorarBtn = document.querySelector('.navbtn-container:nth-child(1) .navbtn');
 const meusFilmesBtn = document.querySelector('.navbtn-container:nth-child(2) .navbtn');
+const novoFilmeBtn = document.querySelector('.novo-filme-btn');
 
-// Seleciona as seções
 const catalogoSection = document.querySelector('.catalogo');
 const meusFilmesSection = document.querySelector('.meus-filmes');
+const novoFilmeSection = document.querySelector('.novo');
 
-// Adiciona eventos de clique nos botões
 explorarBtn.addEventListener('click', () => {
-  // Define qual botão é ativo
   explorarBtn.classList.add('active');
   meusFilmesBtn.classList.remove('active');
 
-  // Exibe o catálogo
   catalogoSection.classList.add('active-section');
   meusFilmesSection.classList.remove('active-section');
+  novoFilmeSection.classList.remove('active-novo');
 });
 
 meusFilmesBtn.addEventListener('click', () => {
-  // Define qual botão é ativo
   meusFilmesBtn.classList.add('active');
   explorarBtn.classList.remove('active');
 
-  // Exibe "Meus Filmes"
   meusFilmesSection.classList.add('active-section');
   catalogoSection.classList.remove('active-section');
+  novoFilmeSection.classList.remove('active-novo');
 });
+
+novoFilmeBtn.addEventListener('click', () => {
+  meusFilmesSection.classList.remove('active-section');
+  novoFilmeSection.classList.add('active-novo');
+});
+
